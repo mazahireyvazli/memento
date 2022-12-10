4044d8c70661c1640a6f51fe09f11b03e8d691b1
mazahireyvazli <mazahir.eyvazli@gmail.com>
2022-12-10 18:37:18 +0100

    introduce test utils

---
+++ utils/test_utils.go
@@ -0,0 +1,82 @@
+package utils
+
+import (
+	"flag"
+	"math/rand"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+	"unsafe"
+)
+
+const (
+	keySize   = 32
+	valueSize = 100
+)
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const (
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
+
+var src = rand.NewSource(time.Now().UnixNano())
+
+// Modified
+// original source: @icza - https://stackoverflow.com/a/31832326/1235621
+func RandBytes(n int) []byte {
+	b := make([]byte, n)
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return b
+}
+
+func RandString(n int) string {
+	b := RandBytes(n)
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+func SkipDiscoveryRun(b *testing.B) bool {
+	benchtime := flag.Lookup("test.benchtime").Value.String()
+
+	return b.N == 1 && strings.HasSuffix(benchtime, "x") && benchtime != "1x"
+}
+
+func PrepareTestData(b *testing.B) map[string][]byte {
+	println("preparing test data")
+
+	data := make(map[string][]byte, b.N)
+
+	for i := 0; i < b.N; i++ {
+		k := RandString(keySize)
+		v := RandBytes(valueSize)
+		data[k] = v
+	}
+
+	println("number of items in test data", len(data))
+
+	return data
+}
+
+func SimpleKey(i int) string {
+	return "key-" + strconv.Itoa(i)
+}
+func ParallelKey(threadID int, counter int) string {
+	return "key-" + strconv.Itoa(threadID) + "-" + strconv.Itoa(counter)
+}
+func SimpleValue() []byte {
+	return make([]byte, valueSize)
+}
