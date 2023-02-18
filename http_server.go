dd2aca818505d0af9537de6168d8f236829f837d
mazahireyvazli <mazahir.eyvazli@gmail.com>
2023-02-18 22:21:10 +0100

    replace gin with chi router

--- http_server.go
+++ http_server.go
@@ -2,9 +2,11 @@ package memento
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
-	"github.com/gin-gonic/gin"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 type MementoServerConfig struct {
@@ -21,37 +23,47 @@ func RunServer(c *MementoServerConfig) error {
 		return err
 	}
 
-	r := gin.Default()
-	apiV1 := r.Group("/api/v1")
+	r := chi.NewRouter()
+	// r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 
-	apiV1.GET("/cache/:key", func(c *gin.Context) {
-		key := c.Param("key")
-		status := http.StatusOK
+	r.Route("/api/v1", func(apiV1 chi.Router) {
+		apiV1.Get("/cache/{key}", func(w http.ResponseWriter, r *http.Request) {
+			key := chi.URLParam(r, "key")
+			status := http.StatusOK
 
-		v, ok := cache.Get(key)
-		if !ok {
-			status = http.StatusNotFound
-		}
+			v, ok := cache.Get(key)
+			if !ok {
+				status = http.StatusNotFound
+			}
 
-		c.Data(status, "text/plain", v)
+			w.Header().Add("Content-Type", "text/plain")
+			w.WriteHeader(status)
+			w.Write(v)
+		})
+
+		apiV1.Put("/cache/{key}/{value}", func(w http.ResponseWriter, r *http.Request) {
+			key := chi.URLParam(r, "key")
+			value := chi.URLParam(r, "value")
+
+			go cache.Set(key, []byte(value))
+
+			w.Header().Add("Content-Type", "text/plain")
+			w.WriteHeader(http.StatusNoContent)
+			w.Write(nil)
+		})
+
+		apiV1.Delete("/cache/:key", func(w http.ResponseWriter, r *http.Request) {
+			key := chi.URLParam(r, "key")
+
+			go cache.Delete(key)
+
+			w.Header().Add("Content-Type", "text/plain")
+			w.WriteHeader(http.StatusNoContent)
+			w.Write(nil)
+		})
 	})
 
-	apiV1.PUT("/cache/:key/:value", func(c *gin.Context) {
-		key := c.Param("key")
-		value := c.Param("value")
-
-		go cache.Set(key, []byte(value))
-
-		c.Data(http.StatusNoContent, "text/plain", nil)
-	})
-
-	apiV1.DELETE("/cache/:key", func(c *gin.Context) {
-		key := c.Param("key")
-
-		go cache.Delete(key)
-
-		c.Data(http.StatusNoContent, "text/plain", nil)
-	})
-
-	return r.Run(fmt.Sprintf("%s:%d", c.Host, c.Port))
+	log.Println("Starting server at", fmt.Sprintf("%s:%d", c.Host, c.Port))
+	return http.ListenAndServe(fmt.Sprintf("%s:%d", c.Host, c.Port), r)
 }
